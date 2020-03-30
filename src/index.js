781729d1a6b9592de919bd68d321e09b970a11a1
fshiper <przemyslaw.krzywodajc@gmail.com>
2020-03-30 20:08:09 +0200

    mutations

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
-}
-`;
-
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+let idCount = links.length
 // 2
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    },
+    updateLink: (parent, args) => {
+      let link = links.find(a => id === args.id)
+    }
   }
 };
 
 // 3
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers
+});
 
-server.start(() => console.log("Server is running on localhost:4000"));
+server.start(() => console.log("Server is running on http://localhost:4000"));
